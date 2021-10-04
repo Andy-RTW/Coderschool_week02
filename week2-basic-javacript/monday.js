c577b3ea18a9b34cd7255845ac428ffd70f00271
Andy-RTW <88776649+Andy-RTW@users.noreply.github.com>
2021-10-05 03:30:45 +0700

    Area of a circle

---
+++ week2-basic-javacript/monday.js
@@ -0,0 +1,11 @@
+console.log('Area of a cirle');
+
+var pi=3.14;
+console.log(pi);
+
+var radius=5;
+console.log(radius);
+
+var area=pi*radius*radius;
+var result="The area of a circle given the radius "+radius+" (m) is "+area+" (m2)";
+console.log(result);
